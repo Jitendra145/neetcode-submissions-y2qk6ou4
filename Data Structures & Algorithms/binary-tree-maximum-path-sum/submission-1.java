7bcbf1fceb6f5fc1cac037636d9b01b544145fbf
Jitendra Verma <jitenverma145@gmail.com>
2026-05-03 22:23:17 +0530

    Add: binary-tree-maximum-path-sum - submission-1

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-1.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        int res[] = {root.val};
+        maxPathSumRec(root,res);
+        return res[0];
+    }
+
+    private int maxPathSumRec(TreeNode root, int[] res){
+        if(root==null)
+            return 0;
+        int l = Math.max(0,maxPathSumRec(root.left,res));
+        int r = Math.max(0,maxPathSumRec(root.right,res));
+
+        res[0] = Math.max(res[0],l+r+root.val);
+
+        return root.val + Math.max(l,r);
+    }
+}
