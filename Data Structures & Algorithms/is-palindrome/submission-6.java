8c8e64538fcdfd381d76c9ca4384dab984a8c3f2
Jitendra Verma <jitenverma145@gmail.com>
2026-06-14 21:50:32 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.toLowerCase();
+        int l=0, r=s.length()-1;
+
+        while(l<r){
+            if(!Character.isLetterOrDigit(str.charAt(l))){
+                l++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(str.charAt(r))){
+                r--;
+                continue;
+            }
+            if(str.charAt(l)!=str.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
