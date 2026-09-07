13cd91d5d0dfd58319d84f8153433a53a860289a
Jitendra Verma <jitenverma145@gmail.com>
2026-09-08 00:26:07 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.java
@@ -0,0 +1,25 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        Queue<Integer> minHeap = new PriorityQueue<>();
+        intervals.sort((int1,int2)->int1.start-int2.start);
+
+        for(Interval interval : intervals){
+            if(!minHeap.isEmpty() && interval.start >=minHeap.peek()){
+                minHeap.poll();
+            }
+            minHeap.offer(interval.end);
+        }
+        return minHeap.size();
+    }
+}
