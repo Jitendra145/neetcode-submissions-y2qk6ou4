6f54307715fbcae2420fcafa2d59b960aaf3be36
Jitendra Verma <jitenverma145@gmail.com>
2026-09-11 00:08:06 +0530

    Add: longest-consecutive-sequence - submission-12

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-12.java
@@ -0,0 +1,21 @@
+class Solution {
+    //Optimal O(n), O(n) neet code
+    public int longestConsecutive(int[] nums) {
+        int res = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        for(int num : nums){
+            if(!set.contains(num-1)){
+                int length =1;
+                while(set.contains(num+length)){
+                    length++;
+                }
+
+                res = Math.max(res,length);
+            }
+        }
+        return res;
+    }
+}
